622aa04c06f54eb04e03c7d9174c4936aaa4f7b7
Sandra <kamsandra07@gmail.com>
2021-07-23 17:13:00 +0200

    com

---
+++ ToDo/Pages/Items/Create.cshtml.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ToDo.Data;
+using ToDo.Models;
+
+namespace ToDo.Pages.Items
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ToDo.Data.ItemContext _context;
+
+        public CreateModel(ToDo.Data.ItemContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Item Item { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Items.Add(Item);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ToDo/Pages/Items/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ToDo.Data;
+using ToDo.Models;
+
+namespace ToDo.Pages.Items
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ToDo.Data.ItemContext _context;
+
+        public IndexModel(ToDo.Data.ItemContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Item> Item { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Item = await _context.Items.ToListAsync();
+        }
+    }
+}
