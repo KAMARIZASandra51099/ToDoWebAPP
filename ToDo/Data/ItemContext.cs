207c83e11044dfb35b82e8f6a08181d2ccd2312d
Sandra <kamsandra07@gmail.com>
2021-07-23 17:09:46 +0200

    Commit

---
+++ ToDo/Data/ItemContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDo.Data
+{
+    public class ItemContext: DbContext
+    {
+        public ItemContext(DbContextOptions<ItemContext> options) : base(options){}
+        public DbSet<Models.Item> Items { get; set; }
+       
+    }
+}
